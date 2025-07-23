00c555b4c8cbd0dabaf7b9c511c1bb250fc69625
Khushal Malhotr <redmi5a3217@gmail.com>
2025-07-23 14:08:26 +0530

    done onboarding-page

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const MainLayout = ({ children }) => {
+  //  redirect to onboarding page
   return (
     <div className="flex justify-center container mx-auto mt-20">
       {children}
